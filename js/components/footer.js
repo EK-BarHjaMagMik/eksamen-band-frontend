16f78e8c8222182779ed1fe825123faa7d0a3604
phaberen <magnusphaberen@gmail.com>
2026-05-11 15:47:03 +0200

    added structure and links for footer

--- js/components/footer.js
+++ js/components/footer.js
@@ -6,8 +6,16 @@ export function renderFooter() {
     const year = now.getFullYear();
     footer.innerHTML = `
     <div class="footer-container">
-        <p>&copy; ${year}. All rights reserved.</p>
-    </div>
+      <h2 class="footer-heading">Contact</h2>
+      <a href="mailto:stuggofficial@gmail.com" class="footer-email">booking@stugg.dk</a>
+      <div class="footer-socials">
+          <a href="https://www.facebook.com/STUGGofficial/" target="_blank" rel="noopener noreferrer">Facebook</a>
+          <a href="https://www.instagram.com/stuggofficial/" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="https://www.youtube.com/@stuggofficial6124" target="_blank" rel="noopener noreferrer">YouTube</a>
+          <a href="https://open.spotify.com/artist/6faOKb101lzOAatluvBUNs" target="_blank" rel="noopener noreferrer">Spotify</a>
+      </div>
+      <p>&copy; ${year}. All rights reserved.</p>
+  </div>
     `;
     
     return footer;
