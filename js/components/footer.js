dfa73fb4c81fc44e0293834dcc916eaf2476a170
phaberen <magnusphaberen@gmail.com>
2026-05-11 16:01:25 +0200

    added icons for href

--- js/components/footer.js
+++ js/components/footer.js
@@ -9,10 +9,11 @@ export function renderFooter() {
       <h2 class="footer-heading">Contact</h2>
       <a href="mailto:stuggofficial@gmail.com" class="footer-email">booking@stugg.dk</a>
       <div class="footer-socials">
-          <a href="https://www.facebook.com/STUGGofficial/" target="_blank" rel="noopener noreferrer">Facebook</a>
-          <a href="https://www.instagram.com/stuggofficial/" target="_blank" rel="noopener noreferrer">Instagram</a>
-          <a href="https://www.youtube.com/@stuggofficial6124" target="_blank" rel="noopener noreferrer">YouTube</a>
-          <a href="https://open.spotify.com/artist/6faOKb101lzOAatluvBUNs" target="_blank" rel="noopener noreferrer">Spotify</a>
+          <a href="https://www.facebook.com/STUGGofficial/" target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-facebook-f"></i></a>
+          <a href="https://www.instagram.com/stuggofficial/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a>
+          <a href="https://www.youtube.com/@stuggofficial6124" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-youtube"></i></a>
+          <a href="https://open.spotify.com/artist/6faOKb101lzOAatluvBUNs" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-spotify"></i></a>
       </div>
       <p>&copy; ${year}. All rights reserved.</p>
   </div>
